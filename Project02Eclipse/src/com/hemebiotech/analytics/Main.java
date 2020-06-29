cc319dee17bb8622c0ae223e4897d347b8b3eaa3
Israel <meir950216@gmail.com>
2020-06-29 10:10:49 +0200

    instanciation d'un objet de la classe AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -3,6 +3,7 @@ package com.hemebiotech.analytics;
 public class Main {
     public static void main(String[] args) {
         ISymptomReader fileReader = new ReadSymptomDataFromFile("symptoms.txt");
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
 
 
     }
