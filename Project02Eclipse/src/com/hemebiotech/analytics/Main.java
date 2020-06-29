f4256a4643f8e1f54020896b9f53883f801d7f20
Israel <meir950216@gmail.com>
2020-06-29 10:14:02 +0200

    instanciation d'une variable de type list*

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,10 +1,13 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ISymptomReader fileReader = new ReadSymptomDataFromFile("symptoms.txt");
         AnalyticsCounter analyticsCounter = new AnalyticsCounter();
-
+        List<String> symptoms;
+        
 
     }
 }
