84616204be928ee19334717ca0681a257be226ee
Israel <meir950216@gmail.com>
2020-06-29 10:03:40 +0200

    la methode countSymptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
@@ -8,5 +9,15 @@ public class AnalyticsCounter {
 	public AnalyticsCounter() {
 		this.symptomsCounter = new TreeMap<String, Integer>();
 	}
-	
+
+	public void countSymtpoms(List<String> symptoms) {
+		for (String symptom : symptoms) {
+			Integer value = symptomsCounter.get(symptom);
+			if (value == null)
+				value = 0;
+			value++;
+			symptomsCounter.put(symptom, value);
+		}
+	}
+
 }
