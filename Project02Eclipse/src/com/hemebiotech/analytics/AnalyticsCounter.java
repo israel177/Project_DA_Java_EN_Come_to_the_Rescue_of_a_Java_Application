f6599086a2b963f4d5ed0c8fa66a116d10ac54b9
Israel <meir950216@gmail.com>
2020-06-29 09:59:58 +0200

    j'ai instanciser une variable de type treemap plus son constructeur

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,8 +1,12 @@
 package com.hemebiotech.analytics;
 
+import java.util.TreeMap;
+
 public class AnalyticsCounter {
+	TreeMap<String, Integer> symptomsCounter;
 
+	public AnalyticsCounter() {
+		this.symptomsCounter = new TreeMap<String, Integer>();
+	}
 	
-
-
 }
