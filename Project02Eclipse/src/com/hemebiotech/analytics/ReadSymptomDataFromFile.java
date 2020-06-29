df79bd6f5d51c229620796b85c438941aceec11b
Israel <meir950216@gmail.com>
2020-06-29 09:52:54 +0200

    dans la methode getSymptom le g etait en masjucule

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -8,46 +8,44 @@ import java.util.List;
 
 /**
  * Simple brute force implementation
- *
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
+    private String filepath;
 
-		String line = null;
+    /**
+     * @param filepath a full or partial path to file with symptom strings in it, one per line
+     */
+    public ReadSymptomDataFromFile(String filepath) {
+        this.filepath = filepath;
+    }
 
-		if (filepath != null) {
-			BufferedReader reader = null;
-			try {
-				reader = new BufferedReader(new FileReader(filepath));
-				line = reader.readLine();
-				while (line != null) {
-					System.out.println("symptom from file: " + line);
-					result.add(line);
-					line = reader.readLine();
-				}
-			} catch (Exception e) {
-				System.out.println(e);
-			} finally {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					System.out.println(e);
-				}
-			}
-		}
-		return result;
-		}
-	}
+    @Override
+    public List<String> getSymptoms() {
+        ArrayList<String> result = new ArrayList<String>();
+
+        String line = null;
+
+        if (filepath != null) {
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new FileReader(filepath));
+                line = reader.readLine();
+                while (line != null) {
+                    System.out.println("symptom from file: " + line);
+                    result.add(line);
+                    line = reader.readLine();
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        return result;
+    }
+}
