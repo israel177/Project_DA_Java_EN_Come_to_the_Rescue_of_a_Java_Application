068a407481d7f6629408ad13a5ccaea9bcec616d
Israel <meir950216@gmail.com>
2020-07-06 11:59:19 +0200

    ajoute de commentaire

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -3,11 +3,13 @@ package com.hemebiotech.analytics;
 import java.util.List;
 
 public class Main {
+    // visibiliter modificateur type de retoure nom et (paramettre) {implementation..return}
     public static void main(String[] args) {
         ISymptomReader fileReader = new ReadSymptomDataFromFile("symptoms.txt");
         AnalyticsCounter analyticsCounter = new AnalyticsCounter();
         List<String> symptoms;
 
+        // appelle de methode et affectation du retour
         symptoms = fileReader.getSymptoms();
         analyticsCounter.countSymtpoms(symptoms);
         analyticsCounter.writeResult();
